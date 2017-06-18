c999aee9e91684993e098eb160789f743ada7872
Conf8house <rin_ruger@mail.ru>
2017-06-18 18:46:53 +1000

    test commit

--- Ms visual C-sharp/Form1.cs
+++ Ms visual C-sharp/Form1.cs
@@ -16,5 +16,12 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1 = "Microsoft Visual C# 12";
+            button1 = "Press my and say hellow";
+                
+        }
     }
 }
